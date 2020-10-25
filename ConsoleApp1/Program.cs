5e1f6684082dfe19cb075c4cd36bb9475cbade0b
best-dim <best-dim@outlook.com>
2020-10-25 19:11:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Работа\net.txt");
+            string tmp = Regex.Replace(text, "[^a-zA-Z ]+", "").Replace("  ", "");
+            string text2 = tmp.Replace(" ", "\n");
+
+            
+
+            System.IO.File.WriteAllText(@"C:\Работа\net2.txt", text2);
+
+        }
+    }
+}
